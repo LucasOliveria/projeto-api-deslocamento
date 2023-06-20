6189e53c72fb3eab45b815c5f94035ec61f2260b
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2023-06-20 09:58:34 -0300

    Primeiro commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from "@/components/Header/styles.module.css"
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Ola</h1>
+    </header>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Header from '@/components/Header'
+import styles from '@/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.main}>
+      <Header />
+    </div>
+  )
+}
